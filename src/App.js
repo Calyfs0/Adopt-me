8a7db4fd849fe140991bb8875235e0820ed9db64
Sagar Gurung <sagarthemaster@gmail.com>
2022-03-03 22:52:44 +0530

    Learning React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Sherry",
+      animal: "Dog",
+      breed: "Pomerarian",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix",
+    }),
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
