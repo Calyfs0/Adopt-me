ec6a9d2540279e2c690dbba278ccc52a9f73d476
Sagar Gurung <sagarthemaster@gmail.com>
2022-03-05 01:34:16 +0530

    Used JSX instead of JS

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,67 @@
 import { useState } from "react";
 
+const Animals = ["dog", "cat", "rabbit", "reptiles"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Dehradun, UK");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
+          {console.log(location)}
           <input
             id="location"
             value={location}
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {Animals.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
